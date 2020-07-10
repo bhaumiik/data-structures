88105f3b0c82df6eca575a2bde7190bd90b25562
Bhaumik Chaudhari <bhaumik.31.10@gmail.com>
2020-07-10 19:10:38 -0400

    Implemented Tree

--- Tree/Tree/Tree.swift
+++ Tree/Tree/Tree.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+public class Tree<Node: Equatable> {
+    public var value: Node
+    public var parent: Tree<Node>?
+    public var children = [Tree<Node>]()
+    
+    /// Initializes Tree `node`.
+    init(value: Node) {
+        self.value = value
+    }
+    
+    /// Adds child to the `node`.
+    /// - Complexity: O(1)
+    public func add(child: Tree<Node>) {
+        child.parent = self
+        self.children.append(child)
+    }
+    
+    /// - Returns: `node` with a given value.
+    /// - Complexity: O(*log n*), where *n* is a number of nodes.
+    public func search(value: Node) -> Tree<Node>? {
+        if self.value == value {
+            return self
+        }
+        for child in self.children {
+            if let foundNode = child.search(value: value) {
+                return foundNode
+            }
+        }
+        return nil
+    }
+}
--- Tree/TreeTests/TreeTests.swift
+++ Tree/TreeTests/TreeTests.swift
@@ -9,25 +9,53 @@
 import XCTest
 
 class TreeTests: XCTestCase {
-
+    var treeRootNode: Tree<String>!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.treeRootNode = Tree<String>(value: "beverages")
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.treeRootNode = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testAddChild() throws {
+        let hotNode = Tree<String>(value: "hot")
+        let coldNode = Tree<String>(value: "cold")
+        self.treeRootNode.add(child: hotNode)
+        self.treeRootNode.add(child: coldNode)
+        XCTAssertTrue(self.treeRootNode.children.count > 0)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testSearchChild() throws {
+        let hotNode = Tree<String>(value: "hot")
+        let coldNode = Tree<String>(value: "cold")
+        
+        let teaNode = Tree<String>(value: "tea")
+        let coffeeNode = Tree<String>(value: "coffee")
+        let cocoaNode = Tree<String>(value: "cocoa")
+        
+        let blackTeaNode = Tree<String>(value: "black")
+        let greenTeaNode = Tree<String>(value: "green")
+        
+        let sodaNode = Tree<String>(value: "soda")
+        let milkNode = Tree<String>(value: "milk")
+        
+        self.treeRootNode.add(child: hotNode)
+        self.treeRootNode.add(child: coldNode)
+        
+        hotNode.add(child: teaNode)
+        hotNode.add(child: coffeeNode)
+        hotNode.add(child: cocoaNode)
+        
+        teaNode.add(child: blackTeaNode)
+        teaNode.add(child: greenTeaNode)
+        
+        coldNode.add(child: sodaNode)
+        coldNode.add(child: milkNode)
+        
+        let node = self.treeRootNode.search(value: "tea")
+        XCTAssertTrue(node?.parent?.value == "hot")
+        XCTAssertTrue(node?.children.count == 2)
     }
-
 }

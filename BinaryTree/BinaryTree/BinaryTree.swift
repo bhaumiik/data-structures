e687cd40c861497562c71c74cbccf2e7ddd7a600
Bhaumik Chaudhari <bhaumik.31.10@gmail.com>
2020-07-11 01:24:38 -0400

    Implemented binary tree

--- BinaryTree/BinaryTree/BinaryTree.swift
+++ BinaryTree/BinaryTree/BinaryTree.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+
+public class BinaryTree<Node> {
+    public var value: Node
+    public var left: BinaryTree<Node>?
+    public var right: BinaryTree<Node>?
+    
+    /// Initialize binary tree.
+    init(left: BinaryTree<Node>?, value: Node, right: BinaryTree<Node>?) {
+        self.left = left
+        self.value = value
+        self.right = right
+    }
+    
+    /// Inserts node to first available space in tree.
+    /// - Complexity: O(*log n*), where *n*  is the number of nodes.
+    func insert(node: BinaryTree<Node>) {
+        var queue = [BinaryTree<Node>]()
+        queue.append(self)
+        while !queue.isEmpty {
+            let temp = queue.removeFirst()
+            if temp.left == nil {
+                temp.left = node
+                break
+            } else {
+                queue.append(temp.left!)
+            }
+            
+            if temp.right == nil {
+                temp.right = node
+                break
+            } else {
+                queue.append(temp.right!)
+            }
+        }
+    }
+    
+    /// - Complexity: O(*log n*), where *n* is the number of nodes.
+    func traverseInOrder(process: (Node) -> Void) {
+        left?.traverseInOrder(process: process)
+        process(value)
+        right?.traverseInOrder(process: process)
+    }
+    
+    /// - Complexity: O(*log n*), where *n* is the number of nodes.
+    func traversePreOrder(process: (Node) -> Void) {
+        process(value)
+        left?.traversePreOrder(process: process)
+        right?.traversePreOrder(process: process)
+    }
+    
+    /// - Complexity: O(*log n*), where *n* is the number of nodes.
+    func traversePostOrder(process: (Node) -> Void) {
+        left?.traversePostOrder(process: process)
+        right?.traversePostOrder(process: process)
+        process(value)
+    }
+}
--- BinaryTree/BinaryTreeTests/BinaryTreeTests.swift
+++ BinaryTree/BinaryTreeTests/BinaryTreeTests.swift
@@ -9,25 +9,68 @@
 import XCTest
 
 class BinaryTreeTests: XCTestCase {
+    var binaryTree: BinaryTree<String>!
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // leaf nodes
+        let nodeA = BinaryTree<String>(left: nil, value: "a", right: nil)
+        let node10 = BinaryTree<String>(left: nil, value: "10", right: nil)
+        let node4 = BinaryTree<String>(left: nil, value: "4", right: nil)
+        let node3 = BinaryTree<String>(left: nil, value: "3", right: nil)
+        let nodeB = BinaryTree<String>(left: nil, value: "b", right: nil)
+        let node5 = BinaryTree<String>(left: nil, value: "5", right: nil)
+        
+        // intermediate nodes on the left
+        let nodeAminus10 = BinaryTree<String>(left: nodeA, value: "-", right: node10)
+        let timesLeft = BinaryTree<String>(left: node5, value: "*", right: nodeAminus10)
+ 
+        // intermediate nodes on the right
+        let nodeMinus4 = BinaryTree<String>(left: node4, value: "-", right: nil)
+        let nodeDivide = BinaryTree<String>(left: node3, value: "/", right: nodeB)
+        let timesRight = BinaryTree<String>(left: nodeMinus4, value: "*", right: nodeDivide)
+        
+        // Root node
+        self.binaryTree = BinaryTree<String>(left: timesLeft, value: "+", right: timesRight)
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.binaryTree = nil
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func testInsert() {
+        self.binaryTree.insert(node: BinaryTree<String>(left: nil, value: "1", right: nil))
+        var node = self.binaryTree
+        while node?.left != nil {
+            node = node?.left
         }
+        XCTAssertTrue(node?.value == "1")
     }
 
+    func testInOrderTraversal() {
+        let inOrderResult = ["5", "*", "a", "-", "10", "+", "4", "-", "*", "3", "/", "b"]
+        var result = [String]()
+        self.binaryTree.traverseInOrder { (value) in
+            result.append(value)
+        }
+        print(result)
+        XCTAssertTrue(result == inOrderResult)
+    }
+    
+    func testPreOrderTraversal() {
+        let preOrderResult = ["+", "*", "5", "-", "a", "10", "*", "-", "4", "/", "3", "b"]
+        var result = [String]()
+        self.binaryTree.traversePreOrder { (value) in
+            result.append(value)
+        }
+        XCTAssertTrue(result == preOrderResult)
+    }
+    
+    func testPostOrderTraversal() {
+        let postOrderResult = ["5", "a", "10", "-", "*", "4", "-", "3", "b", "/", "*", "+"]
+        var result = [String]()
+        self.binaryTree.traversePostOrder { (value) in
+            result.append(value)
+        }
+        XCTAssertTrue(result == postOrderResult)
+    }
 }
